0714826d6578e56c7989b9a7733749eb87e6c068
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 19:05:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   stringArray = string.split(/ /)
   i = 0
   while i < stringArray.length do
-    revArray[i] = stringArray[i].reverse
+    revArray[i] = stringArray[i].to_str.reverse
   end
   p revArray
 end
