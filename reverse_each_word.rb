101875a2b323f376f1fa7a568f0dbf6992fe18ea
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 19:18:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-def reverse_each_word(string)
-  stringArray = string.split(/ /)
-  revArray = []
-  i = 0
-  while i < stringArray.length do
-    revArray[i] = stringArray[i].to_str.reverse
-    i += 1
-  end
-  return revArray.join(" ")
-end
+#def reverse_each_word(string)
+#  stringArray = string.split(/ /)
+#  revArray = []
+#  i = 0
+#  while i < stringArray.length do
+#    revArray[i] = stringArray[i].to_str.reverse
+#    i += 1
+# end
+# return revArray.join(" ")
+#end
